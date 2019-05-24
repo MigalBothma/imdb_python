cc4b7805ecca58e035691c2dbbea068740fd7027
MigalBothma <42865700+MigalBothma@users.noreply.github.com>
2019-05-24 09:49:55 +0200

    docstring added to imdb_helpers

--- imdbscrape/imdb_helpers.py
+++ imdbscrape/imdb_helpers.py
@@ -12,6 +12,9 @@ def remove_empty_arrays(movies_names_wl):
 
 
 def attach_ranks(movies_names_wl):
+    """
+    This function takes movies_names_wl and attaches the rank of index + 1 at the start of the (movie of movies_names_wl) array.
+    """
     for index, movie in enumerate(movies_names_wl):
         if(movie != []):
             movies_names_wl[index].insert(0, index+1)
